700d45b0fe55e710e05eae4a016c957e065fcae0
m <m@bitsnbites.eu>
2017-06-11 23:50:54 +0200

    Add UNLICENSE

--- src/common/time.c
+++ src/common/time.c
@@ -1,3 +1,6 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, see UNLICENSE.
+
 #include "common/time.h"
 
 #if defined(WIN32)
--- src/common/time.h
+++ src/common/time.h
@@ -1,3 +1,6 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, see UNLICENSE.
+
 #ifndef COMMON_TIME_H_
 #define COMMON_TIME_H_
 
